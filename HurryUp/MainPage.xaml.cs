a7e60d2e64762b6e7f4aa6c71836b73e55904722
Popka <p4v3l.pa@yandex.ru>
2022-10-07 23:06:38 +0400

    Добавьте файлы проекта.

---
+++ HurryUp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace HurryUp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void places_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (places.SelectedIndex)
+            {
+                case 0:
+                    roadTimePicker.Time = new TimeSpan(0, 40, 0);
+                    break;
+                case 1:
+                    roadTimePicker.Time = new TimeSpan(1, 10, 0);
+                    break;
+                case 2:
+                    roadTimePicker.Time = new TimeSpan(0, 30, 0);
+                    break;
+                case 3:
+                    roadTimePicker.Time = new TimeSpan(0, 40, 0);
+                    break;
+            }
+        }
+
+        private void btnCalculate_Clicked(object sender, EventArgs e)
+        {
+            TimeSpan preparing = new TimeSpan(0, 40, 0);
+            if ((needTimePicker.Time != TimeSpan.Zero) && (roadTimePicker.Time != TimeSpan.Zero))
+            {
+                TimeSpan startTime = needTimePicker.Time - roadTimePicker.Time - preparing;
+                TimeSpan outTime = needTimePicker.Time - roadTimePicker.Time;
+                answer.Text = $"Необходимо начать  собираться в {startTime} и выйти из дома в {outTime}. При условии что на сборы потребуется {preparing}";
+            }
+        }
+    }
+}
